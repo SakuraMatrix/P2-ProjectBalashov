967823d353b79ac5b7381652eb45eafa35efa8b9
tedbeast <tedbalashov@gmail.com>
2021-08-26 10:44:50 -0400

    added schema file, customer withdraw/deposit

--- Ted/src/main/java/com/github/ProjectBalashov/CustomerRepository.java
+++ Ted/src/main/java/com/github/ProjectBalashov/CustomerRepository.java
@@ -1,8 +1,24 @@
 package com.github.ProjectBalashov;
-import ch.qos.logback.classic.Logger;
 import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.cql.Row;
 import com.datastax.oss.driver.api.core.cql.SimpleStatement;
+import com.datastax.oss.driver.api.querybuilder.QueryBuilder;
+import com.datastax.oss.driver.api.querybuilder.delete.Delete;
+import com.datastax.oss.driver.api.querybuilder.insert.Insert;
+import com.datastax.oss.driver.api.querybuilder.select.Select;
+import com.datastax.oss.driver.api.querybuilder.update.Update;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDate;
+import java.util.Random;
+
+import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.literal;
 
 public class CustomerRepository {
     CqlSession session;
@@ -11,15 +27,87 @@ public class CustomerRepository {
         this.session = session;
         this.log = log;
     }
-    public Customer createUser(Customer customer){
+    public Customer createCustomer(Customer customer){
         log.info("Creating a user: "+customer);
-        SimpleStatement simp = SimpleStatement.builder("INSERT INTO paintingSeller.user"+
-        "(user_id, username, password, balance) " +
-        "values (?, ?, ?, ?)")
-        .addPositionalValues()
+        SimpleStatement simp = SimpleStatement.builder("INSERT INTO Scifi.customer"+
+        "(id, balance) " +
+        "values (?, ?)")
+        .addPositionalValues(customer.getId(), customer.getBalance())
         .build();
         Flux.from(session.executeReactive(simp)).subscribe();
         System.out.println(customer);
         return customer;
+    }
+
+    public Mono<Boolean> deposit(int customerId, double amt) {
+        boolean flag;
+        try {
+            //update account value
+            getCustomerById(customerId).subscribe(customer -> {
+
+                double newBalance = customer.getBalance() + amt;
+                customer.setBalance(newBalance);
+
+                log.info("Attempting to update account balance...");
+                Update updateAccount = QueryBuilder.update("bank", "accounts")
+                        .setColumn("balance", literal(customer.getBalance()))
+                        .whereColumn("account_id").isEqualTo(literal(customerId));
+                log.info("Updated account balance to reflect changes.");
+
+                Flux.just(updateAccount.build())
+                        .flatMap(session::executeReactive)
+                        .subscribe();
+
+            });
+            flag = true;
+        } catch (RuntimeException e) {
+            log.error("Something failed! " + e.getMessage());
+            flag = false;
+        }
+        return Mono.just(flag);
+    }
+
+    public Mono<Boolean> withdraw(int customerId, double amt) {
+        boolean flag;
+        try {
+            //update account value
+            getCustomerById(customerId).subscribe(customer -> {
+
+                if(customer.getBalance() > amt) {
+
+                    double newBalance = customer.getBalance() - amt;
+                    customer.setBalance(newBalance);
+
+                    log.info("Attempting to update account balance...");
+                    Update updateAccount = QueryBuilder.update("bank", "accounts")
+                            .setColumn("balance", literal(customer.getBalance()))
+                            .whereColumn("account_id").isEqualTo(literal(customerId));
+                    log.info("Updated account balance to reflect changes.");
+
+                    Flux.just(updateAccount.build())
+                            .flatMap(session::executeReactive)
+                            .subscribe();
+                }
+                else{
+                    log.info("User did not have enough funds.");
+                }
+
+            });
+            flag = true;
+        } catch (RuntimeException e) {
+            log.error("Something failed! " + e.getMessage());
+            flag = false;
+        }
+        return Mono.just(flag);
+    }
+
+    public Mono<Customer> getCustomerById(int id) {
+        Select select = QueryBuilder.selectFrom("Scifi", "customer")
+                .columns("id", "balance")
+                .whereColumn("id").isEqualTo(literal(id));
+
+        log.info("Getting account by id...");
+        return Mono.from(session.executeReactive(select.build()))
+                .map(row -> new Customer(row.getInt("id"), row.getDouble("balance")));
     }
 }
