de10938042ea72561a9ed5d40500d9e903600341
tedbeast <tedbalashov@gmail.com>
2021-08-26 12:13:51 -0400

    strange happenings

--- Ted/src/main/java/com/github/ProjectBalashov/Customer.java
+++ Ted/src/main/java/com/github/ProjectBalashov/Customer.java
@@ -1,26 +1,7 @@
 package com.github.ProjectBalashov;
 
 public class Customer {
-    int id;
-    double balance;
-    public Customer(int id, double balance){
-        this.id = id;
-        this.balance = balance;
-    }
+    public Customer(){
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(double balance) {
-        this.balance = balance;
     }
 }
