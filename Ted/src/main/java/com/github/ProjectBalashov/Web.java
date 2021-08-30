51cbc0ec37b33d1176d59e2f72ddf488309a45de
tedbeast <tedbalashov@gmail.com>
2021-08-29 20:31:33 -0400

    not done yet just wanted to push sorry

--- Ted/src/main/java/com/github/ProjectBalashov/Web.java
+++ Ted/src/main/java/com/github/ProjectBalashov/Web.java
@@ -3,13 +3,19 @@ package com.github.ProjectBalashov;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import reactor.netty.DisposableServer;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Web{
     public static void main(String[] args){
-
+        SpringApplication.run(Web.class, args);
     }
     static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
     static ByteBuf toByteBuf(Object o) {
