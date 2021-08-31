e6d1b2dcaacce3be680ac4fd59d88c4dea32633f
LanChi Pham <Lpham2525@gmail.com>
2021-08-31 16:14:35 -0400

    tried out webclient with hard coding

--- webflux/src/main/java/com/github/SakuraMatrix/webflux/config/WebConfig.java
+++
@@ -1,9 +0,0 @@
-package com.github.SakuraMatrix.webflux.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.EnableWebFlux;
-
-@Configuration
-@EnableWebFlux
-public class WebConfig {
-}
