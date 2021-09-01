3ebdc2982ff9ad7bfccfa1364384d98779042ad6
sezin demir <87671954+SezinDemir@users.noreply.github.com>
2021-09-01 18:27:04 -0400

    Delete Sezin/src/test/java directory

--- Sezin/src/test/java/com/sakuramatrix/microservicesezin/KarateTest.java
+++
@@ -1,9 +0,0 @@
-package com.sakuramatrix.microservicesezin;
-
-import com.intuit.karate.junit5.Karate;
-
-public class KarateTest {
-  //karatetest.feature file runs good by itself but when I run it within this class it can`t find the .feature file.A bug to fix later.
- @Karate.Test
-    Karate testAll(){return Karate.run().relativeTo(getClass()); }
-}
