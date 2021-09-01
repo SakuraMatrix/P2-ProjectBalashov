2122396caa5ab578451e518d5fbc6c87d569cb48
SezinDemir <sezindemir86@gmail.com>
2021-09-01 00:51:36 -0400

    added webclient

--- Sezin/src/main/java/com/sakuramatrix/microservicesezin/controller/OrdersController.java
+++ Sezin/src/main/java/com/sakuramatrix/microservicesezin/controller/OrdersController.java
@@ -1,10 +1,13 @@
 package com.sakuramatrix.microservicesezin.controller;
-
 import com.sakuramatrix.microservicesezin.domain.Orders;
+import com.sakuramatrix.microservicesezin.domain.Item;
 import com.sakuramatrix.microservicesezin.repository.OrdersRepository;
 import com.sakuramatrix.microservicesezin.service.OrdersService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -13,6 +16,7 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/orders")
 public class OrdersController {
 
+    private static final Logger log = (Logger) LoggerFactory.getLogger(OrdersController.class);
     private OrdersService ordersService;
 
     public OrdersController(OrdersService ordersService) {
@@ -24,28 +28,34 @@ public class OrdersController {
 
 
     @GetMapping("/{customer_id}")
-    public Flux<Orders> getById(@PathVariable int customer_id){
+        public Flux<Orders> getById(@PathVariable int customer_id){
+        log.info("Finding order by customer id");
         return ordersRepository.findById(customer_id);
     }
 
     @GetMapping("/all")
     public Flux<Orders> getAllOrders(){
+        log.info("Finding all orders ");
         return  ordersRepository.findAll();
     }
 
     @PostMapping("/{customer_id}")
     public Mono<Orders> saveById (@RequestBody Orders orders) {
+        log.info("Saving an order by customer id ");
         return ordersService.saveById(orders);
     }
 
     @PutMapping("/update/{customer_id}")
     public Flux<Orders> updateById (@PathVariable int customer_id, @RequestBody Orders orders) {
+        log.info("Updating an order by customer id ");
         return ordersService.updateById (customer_id, orders);
     }
 
     @DeleteMapping("/{customer_id}")
     public void delete (@PathVariable int customer_id) {
+        log.info("Deleting an order by customer id ");
         ordersService.deleteById (customer_id);
+
     }
 }
 
